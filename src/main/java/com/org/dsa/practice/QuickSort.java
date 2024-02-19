d0ba31dcb191f2471f304c72c9ffe742b5d87f22
sainadhmanyam <76801368+sainadhmanyam@users.noreply.github.com>
2024-02-19 10:10:36 -0500

    quick sort

--- src/main/java/com/org/dsa/practice/QuickSort.java
+++ src/main/java/com/org/dsa/practice/QuickSort.java
@@ -1,4 +1,36 @@
 package com.org.dsa.practice;
 
 public class QuickSort {
+    public static void main(String[] args) {
+        int arr[] = {5,6,2,3,1,8,4};
+        quickSort(arr, 0, arr.length-1);
+        for (int temp:arr) {
+            System.out.print(temp+" ");
+        }
+    }
+
+    private static void quickSort(int[] arr, int low, int high) {
+        if(low<high){
+            int pivot = partition(arr, low, high);
+            quickSort(arr,low, pivot-1);
+            quickSort(arr,pivot+1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low-1;
+        for(int j = low; j<high; j++){
+            if (arr[j] < pivot){
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+        return i+1;
+    }
 }
